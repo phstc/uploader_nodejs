d2929c438d3cc1a23afce4620fbed59f23125d90
Pablo Cantero <pablo@pablocantero.com>
2012-06-14 11:23:53 -0300

    Serving static files

--- uploader/app.js
+++ uploader/app.js
@@ -2,18 +2,27 @@ var app = require('http').createServer(handler)
 	, io = require('socket.io').listen(app)
 	, formidable = require('formidable')
 	, fs = require('fs')
-	, url = require('url');
+	, url = require('url')
+	, static = require('node-static');
+	
 
 var port = (process.argv[2] == 'production') ? 80 : 8080;
 
 app.listen(port);
+
+var fileServer = new(static.Server)('./public');
 
 function handler (req, res) {
 	if (req.url.match(/^\/upload.+/) && req.method.toLowerCase() == 'post') {
 		upload(req, res);
 		return;
 	}
-	index(req, res);
+	fileServer.serve(req, res, function (err, result) {
+		if (err) {
+			res.writeHead(err.status, err.headers);
+			res.end();
+		}
+	});
 }
 
 function sendSocketMessage(socketid, room, message){
@@ -25,7 +34,7 @@ function sendSocketMessage(socketid, room, message){
 function upload(req, res){
 	var form = new formidable.IncomingForm();
 
-	form.uploadDir = __dirname + '/uploaded_files';
+	form.uploadDir = __dirname + '/public/uploaded_files';
 
 	form.parse(req, function(err, fields, files) {
 		res.writeHead(200, {'Content-type': 'text/plain'});
@@ -34,7 +43,7 @@ function upload(req, res){
 
 	form.on('file', function(field, file) {
 		fs.rename(file.path, form.uploadDir + '/' + file.name);
-		sendSocketMessage(url_parts.query.socketid, 'uploaded_file', '/uploaded_files/' + '/' + file.name);
+		sendSocketMessage(url_parts.query.socketid, 'uploaded_file', {path: '/uploaded_files/' + file.name, name: file.name});
 	});
 
 	var url_parts = url.parse(req.url, true);
@@ -42,12 +51,6 @@ function upload(req, res){
 	form.addListener('progress' , function(bytesReceived, bytesExpected){
 		var percentage = parseInt((bytesReceived * 100) / bytesExpected, 10);
 		sendSocketMessage(url_parts.query.socketid, 'percentage', percentage);
-	});
-}
-
-function index(req, res){
-	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
-		res.end(text);
 	});
 }
 
