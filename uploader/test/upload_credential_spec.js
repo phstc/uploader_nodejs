2d9c28ed7f5a1811fe7eff70cde297398635825d
Pablo Cantero <pablo@pablocantero.com>
2012-06-17 21:25:15 -0300

    Adding Upload Credential to validate incomings uploads

---
+++ uploader/test/upload_credential_spec.js
@@ -0,0 +1,16 @@
+require('should');
+
+var uploadCredential = require('../upload_credential');
+
+describe('Upload Credential', function(){
+  describe('#create', function(){
+    it('should create a MD5 hash', function(){
+      uploadCredential.create('1339976490').should.equal('873d7ae7d18c3b072bf88d2abc1d6464');
+    });
+  });
+  describe('#isValid', function(){
+    it('should return true for a valid credential', function(){
+      uploadCredential.isValid('873d7ae7d18c3b072bf88d2abc1d6464', '1339976490').should.be.true;
+    });
+  });
+});
---
+++ uploader/upload_credential.js
@@ -0,0 +1,11 @@
+var SECRET_KEY = 'BgIPIUgDT1wOwyKQN9Mp';
+
+var crypto = require('crypto');
+
+exports.create = function(timestamp){
+	return crypto.createHash('md5').update(SECRET_KEY + timestamp).digest('hex');
+};
+
+exports.isValid = function(securityHash, timestamp){
+	return exports.create(timestamp) == securityHash;
+};
